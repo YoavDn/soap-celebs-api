0e1236c3d3ccc95ba44a638a61816018b73b1dd5
yoav <yoavdan.m@gmail.com>
2022-11-02 19:26:39 +0200

    ts node env setup

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log('hello')
+import express from 'express'
+import path from 'path'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import { Request, Response, NextFunction } from 'express'
+
+const app = express()
+const port = process.env.PORT || 8080
+
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('hello world')
+})
+
+app.listen(port, () => {
+    console.log('app listening on port ' + port)
+})
