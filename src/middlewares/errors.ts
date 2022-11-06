ce1ea05e09878545da30e58ab247ad3fe6c7109a
yoav <yoavdan.m@gmail.com>
2022-11-06 14:00:03 +0200

    added morgan and notfound middleware

---
+++ src/middlewares/errors.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express'
+
+
+interface Error {
+    code?: number
+    status?: number
+}
+
+function notFound(req: Request, res: Response, next: NextFunction) {
+    const err = new Error('There is nothing here.') as Error
+    err.status = 404
+    next(err)
+}
+
+export const errors = {
+    notFound,
+}
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
+import cors from 'cors'
+import morgan from 'morgan'
 import config from './config'
 import routes from './routes'
+import { errors } from './middlewares/errors'
 
 const app = express()
 const port = process.env.PORT || 8080
 
+app.use(morgan('tiny'))
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -15,6 +20,8 @@ mongoose.connect(config.dbUrl)
     .catch((err) => console.log('failed to connect', err))
 
 app.use('/api/', routes)
+
+app.use(errors.notFound)
 
 app.listen(port, () => {
     console.log('app listening on port ' + port)
