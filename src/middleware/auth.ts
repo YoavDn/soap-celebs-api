5b67f0e6abf87d6e3c534d9968f28bd45df9577a
yoav <yoavdan.m@gmail.com>
2022-11-08 14:51:31 +0200

    added auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,7 +1,11 @@
 import { Request, Response, NextFunction } from 'express'
 
 function isDev(req: Request, res: Response, next: NextFunction) {
-    if (process.env.NODE_ENV === 'development') next()
+    if (process.env.NODE_ENV !== 'development') {
+        res.status(401)
+        return res.send('Not authorized')
+    }
+    next()
 }
 
 export const auth = {
